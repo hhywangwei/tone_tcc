33053740b550ae8b7ee59bf397843a64e88010ba
hhywangwei <hhywangwei@gmail.com>
2014-01-01 16:24:24 +0800

    实现客户端调用方法

--- tone_tcc_cti/tcc_cti_server/src/main/java/com/tcc/cti/web/server/CtiServerFac.java
+++ tone_tcc_cti/tcc_cti_server/src/main/java/com/tcc/cti/web/server/CtiServerFac.java
@@ -11,14 +11,36 @@ import org.springframework.stereotype.Service;
 import com.tcc.cti.driver.Configure;
 import com.tcc.cti.driver.Operator;
 import com.tcc.cti.driver.message.RequestTimeoutException;
+import com.tcc.cti.driver.message.request.CallHelpRequest;
+import com.tcc.cti.driver.message.request.CallHoldRequest;
+import com.tcc.cti.driver.message.request.CallRequest;
+import com.tcc.cti.driver.message.request.GroupMemberRequest;
+import com.tcc.cti.driver.message.request.GroupRequest;
 import com.tcc.cti.driver.message.request.LoginRequest;
 import com.tcc.cti.driver.message.request.LogoutRequest;
+import com.tcc.cti.driver.message.request.MobileNumberCancelRequest;
+import com.tcc.cti.driver.message.request.MobileNumberRequest;
+import com.tcc.cti.driver.message.request.MonitorRequest;
+import com.tcc.cti.driver.message.request.OutCallCancelRequest;
+import com.tcc.cti.driver.message.request.OutCallRequest;
+import com.tcc.cti.driver.message.request.OwnRequest;
+import com.tcc.cti.driver.message.request.PasswordRequest;
 import com.tcc.cti.driver.message.request.Requestable;
+import com.tcc.cti.driver.message.request.RestRequest;
+import com.tcc.cti.driver.message.request.ResumeRequest;
+import com.tcc.cti.driver.message.request.SilenceRequest;
+import com.tcc.cti.driver.message.request.TransferGroupRequest;
+import com.tcc.cti.driver.message.request.TransferOneRequest;
 import com.tcc.cti.driver.message.response.Response;
 import com.tcc.cti.driver.session.SessionFactory;
 import com.tcc.cti.driver.session.SessionRegisterException;
 import com.tcc.cti.driver.session.Sessionable;
 
+/**
+ * 实现{@link CtiServerFacable}接口,该实现依赖spring框架。
+ * 
+ * @author <a href="hhywangwei@gmail.com">WangWei</a>
+ */
 @Service
 public class CtiServerFac implements CtiServerFacable,InitializingBean {
 	private static final Logger logger = LoggerFactory.getLogger(CtiServerFac.class);
@@ -45,6 +67,202 @@ public class CtiServerFac implements CtiServerFacable,InitializingBean {
 			Sessionable session = getSession(operator);			
 			send(session,r);
 			return responseSingle(r);
+	}
+
+	@Override
+	public Response logout(Operator operator)throws CtiServerException{
+		Sessionable session = getSession(operator);
+		LogoutRequest r = new LogoutRequest();
+		send(session,r);
+		return responseSingle(r);
+	}
+	
+	@Override
+	public List<Response> getGroups(Operator operator, String groupId)
+			throws CtiServerException {
+		Sessionable session = getSession(operator);
+		GroupRequest r = new GroupRequest();
+		r.setGroupId(groupId);
+		send(session,r);
+		return response(r);
+	}
+	
+	@Override
+	public List<Response> getGroupMembers(Operator operator, String groupId)
+			throws CtiServerException {
+		Sessionable session = getSession(operator);
+		GroupMemberRequest r = new GroupMemberRequest();
+		r.setGroupId(groupId);
+		send(session,r);
+		return response(r);
+	}
+
+	@Override
+	public Response getOwnInfo(Operator operator) throws CtiServerException {
+		Sessionable session = getSession(operator);
+		OwnRequest r = new OwnRequest();
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response getWorkInfo(Operator operator, String workId)
+			throws CtiServerException {
+		Sessionable session = getSession(operator);
+		OwnRequest r = new OwnRequest();
+		r.setWorkId(workId);
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public List<Response> getMonitors(Operator operator)
+			throws CtiServerException {
+		Sessionable session = getSession(operator);
+		MonitorRequest r = new MonitorRequest();
+		send(session,r);
+		return response(r);
+	}
+
+	@Override
+	public List<Response> getCall(Operator operator) throws CtiServerException {
+		Sessionable session = getSession(operator);
+		CallRequest r = new CallRequest();
+		send(session,r);
+		return response(r);
+	}
+
+	@Override
+	public Response outCall(Operator operator, String opNumber,
+			String callNumber) throws CtiServerException {
+		Sessionable session = getSession(operator);
+		OutCallRequest r = new OutCallRequest();
+		r.setOpNumber(opNumber);
+		r.setPhone(callNumber);
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response outCallCancel(Operator operator) throws CtiServerException {
+		Sessionable session = getSession(operator);
+		OutCallCancelRequest r = new OutCallCancelRequest();
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response mobileNumber(Operator operator, String number)
+			throws CtiServerException {
+		Sessionable session = getSession(operator);
+		MobileNumberRequest r = new MobileNumberRequest();
+		r.setNumber(number);
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response mobileNumberCancel(Operator operator)
+			throws CtiServerException {
+		Sessionable session = getSession(operator);
+		MobileNumberCancelRequest r = new MobileNumberCancelRequest();
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response changePassword(Operator operator, String password)
+			throws CtiServerException {
+		Sessionable session = getSession(operator);
+		PasswordRequest r = new PasswordRequest();
+		r.setPassword(password);
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response resum(Operator operator) throws CtiServerException {
+		Sessionable session = getSession(operator);
+		ResumeRequest r = new ResumeRequest();
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response rest(Operator operator) throws CtiServerException {
+		Sessionable session = getSession(operator);
+		RestRequest r = new RestRequest();
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response silence(Operator operator) throws CtiServerException {
+		Sessionable session = getSession(operator);
+		SilenceRequest r = new SilenceRequest();
+		r.setFlag("1");
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response closeSilence(Operator operator) throws CtiServerException {
+		Sessionable session = getSession(operator);
+		SilenceRequest r = new SilenceRequest();
+		r.setFlag("0");
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response callHold(Operator operator) throws CtiServerException {
+		Sessionable session = getSession(operator);
+		CallHoldRequest r = new CallHoldRequest();
+		r.setFlag("1");
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response closeCallHold(Operator operator) throws CtiServerException {
+		Sessionable session = getSession(operator);
+		CallHoldRequest r = new CallHoldRequest();
+		r.setFlag("0");
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response callHelp(Operator operator, String workId, String number,
+			String flag) throws CtiServerException {
+		Sessionable session = getSession(operator);
+		CallHelpRequest r = new CallHelpRequest();
+		r.setTransferNumber(number);
+		r.setTransferWorkId(workId);
+		r.setStatus(flag);
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response transferGroup(Operator operator, String groupId)
+			throws CtiServerException {
+		Sessionable session = getSession(operator);
+		TransferGroupRequest r = new TransferGroupRequest();
+		r.setGroupId(groupId);
+		send(session,r);
+		return responseSingle(r);
+	}
+
+	@Override
+	public Response transferOne(Operator operator, String workId, String number)
+			throws CtiServerException {
+		Sessionable session = getSession(operator);
+		TransferOneRequest r = new TransferOneRequest();
+		r.setWorkId(workId);
+		r.setNumber(number);
+		send(session,r);
+		return responseSingle(r);
 	}
 	
 	private Sessionable getSession(Operator operator)throws CtiServerException{
@@ -81,13 +299,20 @@ public class CtiServerFac implements CtiServerFacable,InitializingBean {
 			throw new CtiServerException(e);
 		}
 	}
-
-	@Override
-	public Response logout(Operator operator)throws CtiServerException{
-		Sessionable session = getSession(operator);
-		LogoutRequest r = new LogoutRequest();
-		send(session,r);
-		return responseSingle(r);
+	
+	@SuppressWarnings("unchecked" )
+	private List<Response> response(Requestable<? extends Response> request)throws CtiServerException{
+		try{
+			return (List<Response>)request.response();
+		}catch(RequestTimeoutException e){
+			logger.debug("{} request timeout,Error is {}",
+					request.toString(),e.toString());
+			throw new CtiServerException(e);
+		}catch(InterruptedException e){
+			logger.debug("{} interrupted exception,Error is {}",
+					request.toString(),e.toString());
+			throw new CtiServerException(e);
+		}
 	}
 	
 	public void setHost(String host){
@@ -97,4 +322,5 @@ public class CtiServerFac implements CtiServerFacable,InitializingBean {
 	public void setPort(int port){
 		_port = port;
 	}
+
 }
--- tone_tcc_cti/tcc_cti_server/src/main/java/com/tcc/cti/web/server/CtiServerFacable.java
+++ tone_tcc_cti/tcc_cti_server/src/main/java/com/tcc/cti/web/server/CtiServerFacable.java
@@ -1,5 +1,7 @@
 package com.tcc.cti.web.server;
 
+import java.util.List;
+
 import com.tcc.cti.driver.Operator;
 import com.tcc.cti.driver.message.response.Response;
 
@@ -17,8 +19,8 @@ public interface CtiServerFacable {
 	 * @param password 密码
 	 * @param opNumber 登录电话
 	 * @param type 座席类型
-	 * @exception {@link CtiServerException}
 	 * @return 返回消息对象
+	 * @throws CtiServerException
 	 */
 	Response login(Operator operator,String password,String opNumber,String type)throws CtiServerException;
 	
@@ -26,10 +28,206 @@ public interface CtiServerFacable {
 	 * 退出CTI服务器
 	 * 
 	 * @param operator 操作员信息
-	 * @exception {@link CtiServerException}
 	 * @return 返回消息对象
+	 * @throws  CtiServerException
 	 */
 	Response logout(Operator operator)throws CtiServerException;
 	
+	/**
+	 * 得到分组信息
+	 * 
+	 * @param operator 操作员对象
+	 * @param groupId 查询指定的用户组编号，{@value null}时得到所有分组信息
+	 * @return  返回{@link GroupResponse}分组对象列表
+	 * @throws CtiServerException
+	 */
+	List<Response> getGroups(Operator operator,String groupId)throws CtiServerException;
 	
+	/**
+	 * 得到分组成员信息
+	 * 
+	 * @param operator 操作员对象
+	 * @param groupId 用户组编号
+	 * @return 返回{@link GroupMemberReqsponse}用户组成员消息对象
+	 * @throws CtiServerException
+	 */
+	List<Response> getGroupMembers(Operator operator,String groupId)throws CtiServerException;
+	
+	/**
+	 * 得到本座席信息
+	 * 
+	 * @param operator 操作员对象
+	 * @return 返回{@link OwnResponse}对象
+	 * @throws CtiServerException
+	 */
+	Response getOwnInfo(Operator operator)throws CtiServerException;
+	
+	/**
+	 * 得到座席信息
+	 * 
+	 * @param operator 操作员对象
+	 * @param workId 工号,{@value null}时查询自己信息,<i>注意：工号不是"opID"，可以在{@link getGroupMembers}得到用户工号</i>
+	 * @return 返回{@link OwnResponse}对象
+	 * @throws CtiServerException
+	 */
+	Response getWorkInfo(Operator operator,String workId)throws CtiServerException;
+	
+	/**
+	 * 得到班长信息列表
+	 *  
+	 * @param operator 操作员对象
+	 * @return {@link MonitorResponse}对象
+	 * @throws CtiServerException
+	 */
+	List<Response> getMonitors(Operator operator)throws CtiServerException;
+	
+	/**
+	 * 得到电话呼叫信息
+	 * 
+	 * <pre>
+	 * 这个方法非常重要，通过改方法客户可以得到电话状态，如：“震铃”、“接通”等。客户端可使用"Long poll"方式来调用。
+	 * <i>注意：客户端是否存活也依赖该方法。</i>
+	 * </pre>
+	 * 
+	 * @param operator
+	 * @return
+	 * @throws CtiServerException
+	 */
+	List<Response> getCall(Operator operator)throws CtiServerException;
+	
+	/**
+	 * 座席外呼请求
+	 * 
+	 * @param operator 操作员编号
+	 * @param opNumber 座席号码
+	 * @param callNumber 呼叫号码
+	 * @return {@link OutCallStateResponse}对象
+	 * @throws CtiServerException
+	 */
+	Response outCall(Operator operator,String opNumber,String callNumber)throws CtiServerException;
+	
+	/**
+	 * 取消外呼
+	 * 
+	 * @param operator 操作员对象
+	 * @return 
+	 * @throws CtiServerException
+	 */
+	Response outCallCancel(Operator operator)throws CtiServerException;
+	
+	/**
+	 * 设置移动座席号码
+	 * 
+	 * @param operator 操作员对象
+	 * @param number 移动座席号码
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response mobileNumber(Operator operator,String number)throws CtiServerException;
+	
+	/**
+	 * 移动座席号码取消
+	 * 
+	 * @param operator 操作员对象
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response mobileNumberCancel(Operator operator)throws CtiServerException;
+	
+	/**
+	 * 改变用户密码
+	 * 
+	 * @param operator 操作员对象
+	 * @param password 密码
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response changePassword(Operator operator,String password)throws CtiServerException;
+	
+	/**
+	 * 请求休息
+	 *  
+	 * @param operator 操作员对象
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response resum(Operator operator)throws CtiServerException;
+	
+	/**
+	 * 恢复工作
+	 * 
+	 * @param operator 操作员对象
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response rest(Operator operator)throws CtiServerException;
+	
+	/**
+	 * 静音
+	 * 
+	 * @param operator 操作员对象
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response silence(Operator operator)throws CtiServerException;
+	
+	/**
+	 * 关闭静音
+	 * 
+	 * @param operator 操作员对象
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response closeSilence(Operator operator)throws CtiServerException;
+	
+	/**
+	 * 保存通话
+	 * 
+	 * @param operator 操作员对象
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response callHold(Operator operator)throws CtiServerException;
+	
+	/**
+	 * 关闭保存通话
+	 * 
+	 * @param operator 操作员对象
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response closeCallHold(Operator operator)throws CtiServerException;
+	
+	/**
+	 * 通话帮助
+	 * 
+	 * @param operator 操作员对象
+	 * @param workId 帮助工号
+	 * @param number 帮助号码
+	 * @param flag 0－求助，1－三方会议
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response callHelp(Operator operator,String workId,String number,String flag)throws CtiServerException;
+	
+	/**
+	 * 转接到组
+	 * 
+	 * @param operator 操作员对象
+	 * @param groupId 组编号
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response transferGroup(Operator operator,String groupId)throws CtiServerException;
+	
+	/**
+	 * 转接到某一用户
+	 * 
+	 * @param operator 操作员对象
+	 * @param workId 工作编号
+	 * @param number 呼叫号码
+	 * @return
+	 * @throws CtiServerException
+	 */
+	Response transferOne(Operator operator,String workId,String number)throws CtiServerException;
 }
